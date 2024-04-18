5c41c5cd98bf0f6d491ed4a0bca2f343eb865178
Your Name <you@example.com>
2024-04-18 14:30:36 +0200

    changes

--- src/layouts/Home Page/Carousel.tsx
+++ src/layouts/Home Page/Carousel.tsx
@@ -1,6 +1,8 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
-    return(
-        <div className='container mt-5' style={{height: 550}}>
+    return (
+        <div className='container mt-5' style={{ height: 550 }}>
             <div className='homepage-carousel-title'>
                 <h3>Find your next "I stayed up too late reading" book.</h3>
             </div>
@@ -11,90 +13,46 @@ export const Carousel = () => {
                 <div className='carousel-inner'>
                     <div className='carousel-item active'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                           
-                           
-                           <div className='carousel-item'></div>
-                            <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-                                <div className='text-center'>
-                                    <img 
-                                        src={require('./../../Images/BooksImages/book-luv2code-1000.png')} 
-                                        width='151'
-                                        height='233'
-                                        alt="book"
-                                         />
-                                         <h6 className='mt-2'>Book</h6>
-                                         <p>Luv2Code</p>
-                                         <a className='btn main-color text-white' href='#'>Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook/>
+                            <ReturnBook/>
                         </div>
                     </div>
                     <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-                                <div className='text-center'>
-                                    <img 
-                                        src={require('./../../Images/BooksImages/book-luv2code-1000.png')} 
-                                        width='151'
-                                        height='233'
-                                        alt="book"
-                                         />
-                                         <h6 className='mt-2'>Book</h6>
-                                         <p>Luv2Code</p>
-                                         <a className='btn main-color text-white' href='#'>Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook/>
+                            <ReturnBook/>
                         </div>
-                        <div className='carousel-item'>
+                    </div>
+                    <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-                                <div className='text-center'>
-                                    <img 
-                                        src={require('./../../Images/BooksImages/book-luv2code-1000.png')} 
-                                        width='151'
-                                        height='233'
-                                        alt="book"
-                                         />
-                                         <h6 className='mt-2'>Book</h6>
-                                         <p>Luv2Code</p>
-                                         <a className='btn main-color text-white' href='#'>Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook/>
+                            <ReturnBook/>
                         </div>
                     </div>
-                    </div>
-                    <button className='carousel-contol-prev' type='button'
-                        data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
-                        <span className='varousel-control-prev-icon' aria-hidden='true'></span>
-                        <span className='visually-hidden'>Previous</span>
-                    </button>
-                    <button className='carousel-contol-next' type='button'
-                        data-bs-target='#carouselExampleControls' data-bs-slide='next'>
-                        <span className='varousel-control-next-icon' aria-hidden='true'></span>
-                        <span className='visually-hidden'>Next</span>
-                    </button>
                 </div>
+                <button className='carousel-contol-prev' type='button'
+                    data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                    <span className='varousel-control-prev-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Previous</span>
+                </button>
+                <button className='carousel-contol-next' type='button'
+                    data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+                    <span className='varousel-control-next-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Next</span>
+                </button>
+            </div>
 
-                {/* Mobile */}
-                <div className='d-lg-none-mt3'>
-                    <div className='row d-flex justify-content-center align-items-center'>
-                        <div className='text-center'>
-                            <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                width='151'
-                                height='233'
-                                alt="book"
-                             />
-                             <h6 className='mt-2'>
-                                <b>Book</b>
-                             </h6>
-                             <p>Luv2Code</p>
-                             <a className='btn main-color text-white' href='#'>Reserve</a>
-                        </div>
-                    </div>
+            {/* Mobile */}
+            <div className='d-lg-none-mt3'>
+                <div className='row d-flex justify-content-center align-items-center'>
+                    <ReturnBook/>
                 </div>
-                <div className='homepage-carousel-title mt-3'>
-                    <a className='btn btn-outline-secondary btn-lg' href='#'>View More</a>
-                </div>
+            </div>
+            <div className='homepage-carousel-title mt-3'>
+                <a className='btn btn-outline-secondary btn-lg' href='#'>View More</a>
             </div>
         </div>
     );
